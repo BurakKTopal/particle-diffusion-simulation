1515c16af2770e81ee245ffd612810e164b553cb
BKtopal <burak.kucuktopal123@gmail.com>
2024-12-15 22:42:29 +0100

    cleans up unused code

--- src/SimulationParams.cpp
+++
@@ -1,7 +0,0 @@
-#include "../inc/SimulationParams.h"
-
-int n = 100;              // Grid size
-float gridSpacing = 1.0f; // Spacing between grid lines
-float cameraX = 0.0f;     // Camera X position
-float cameraY = 0.0f;     // Camera Y position
-float zoom = 1.0f;        // Zoom level
